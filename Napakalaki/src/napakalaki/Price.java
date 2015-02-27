662c61db6d834df9652f1b08e84c51abfe3ac7a1
Salva <Salva@172.20.96.63>
2015-02-27 12:54:57 +0100

    Creo Price

---
+++ Napakalaki/src/napakalaki/Price.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author Salva
+ */
+public class Price {
+    private int subidaNivel;//numero de niveles que sube con el buen royo
+    private int tesoros;//numero de tesoros que consigue al vencer
+    
+    
+}
