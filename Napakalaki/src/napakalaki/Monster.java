abc7d57ca661f13397c1269b8326cbce67f6c325
xehartnort <xehartnort@xehartnort-HP-Pavilion-Sleekbook-15>
2015-02-28 00:24:50 +0100

    Nueva clase Monster, dispone de de getters y constructor con parámetros.

---
+++ Napakalaki/src/napakalaki/Monster.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author xehartnort
+ */
+public class Monster {
+    private String name;
+    private int combatLevel;
+    private Prize price;
+    private BadConsequence bc;
+    
+    public Monster(String name, int level, 
+                    BadConsequence bc, 
+                    Prize price)
+    {
+        this.name = name;
+        this.combatLevel = level;
+        this.price = price;
+        this.bc = bc;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public int getCombatLevel()
+    {
+        return combatLevel;
+    }
+}
