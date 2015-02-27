b1d8e68ebc8b409053f839cf3a042619743ec5d4
Salva <Salva@172.20.96.63>
2015-02-27 13:13:28 +0100

    Constructores y metosdos liston en Prize

---
+++ Napakalaki/src/napakalaki/Prize.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author Salva
+ */
+public class Prize {
+    private int levels;//numero de niveles que sube con el buen royo
+    private int treasures;//numero de tesoros que consigue al vencer
+    /**
+     * @
+    */
+    
+    public Prize(int treasures, int levels)
+    {
+        this.levels= levels;
+        this.treasures=treasures;
+    }
+    
+    public int getLevels(){
+        return levels;
+    }
+    public int getTreasures(){
+        return treasures;
+    }
+}
