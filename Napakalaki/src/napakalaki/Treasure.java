4bea8c8636d3212aa253dd8085e9c7319f7b1c02
xehartnort <xehartnort@xehartnort-HP-Pavilion-Sleekbook-15>
2015-03-22 12:52:05 +0100

    Creada clase de acuerdo con la práctica 2, sesión 1.

---
+++ Napakalaki/src/napakalaki/Treasure.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author xehartnort
+ */
+public class Treasure {
+    private String name;
+    private int goldBonus;
+    private int minBonus;
+    private int maxBonus;
+    private TreasureKind type;
+    
+    public Treasure(String n, int g, int min, int max, TreasureKind t)
+    {
+        this.name = n;
+        this.goldBonus = g;
+        this.minBonus = min;
+        this.maxBonus = max;
+        this.type = t;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getGoldCoins()
+    {
+        return goldBonus;
+    }
+
+    public int getMinBonus()
+    {
+        return minBonus;
+    }
+
+    public int getMaxBonus()
+    {
+        return maxBonus;
+    }
+    
+    public TreasureKind getType()
+    {
+        return type;
+    }
+}
