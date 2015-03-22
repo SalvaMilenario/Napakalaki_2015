d89414576e09a8958020d7e56470145f6383cbea
xehartnort <xehartnort@xehartnort-HP-Pavilion-Sleekbook-15>
2015-03-22 12:49:12 +0100

    Creada enumeración de acuerdo con la práctica 2, sesión 1.

---
+++ Napakalaki/src/napakalaki/CombatResult.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author xehartnort
+ *///gana y gana el juego, gana, pierde, pierde y escapa, pierde y muere
+public enum CombatResult {WINANDWINGAME, WIN, LOSE, LOSEANDESCAPE, LOSEANDDIE};
