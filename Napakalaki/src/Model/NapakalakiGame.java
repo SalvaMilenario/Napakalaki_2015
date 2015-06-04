dba3c9ca27a96bb5d4539becf7d9dac377f317d5
xehartnort <xehartnort@optimus>
2015-06-04 12:42:19 +0200

    Nuevo nombre de la clase que contiene al main del juego

---
+++ Napakalaki/src/Model/NapakalakiGame.java
@@ -0,0 +1,31 @@
+
+package Model;
+
+import GUI.NapakalakiView;
+import GUI.PlayerNamesCapture;
+import java.util.ArrayList;
+//import Test.GameTester;
+
+public class NapakalakiGame {
+
+    public static void main(String[] args) {
+      Napakalaki napakalakiModel = Napakalaki.getInstance();
+      NapakalakiView napakalakiView = new NapakalakiView();
+      
+      Dice.createInstance (napakalakiView); //instancia del dado gráfico
+      napakalakiView.setNapakalaki(napakalakiModel); //se enlaza el modelo(Napakalaki) a la vista (NapakalakiView)
+
+      ArrayList<String> names;
+      PlayerNamesCapture namesCapture = new PlayerNamesCapture(napakalakiView, true);
+      names = namesCapture.getNames();
+      
+      napakalakiModel.initGame(names);
+      napakalakiView.showView(); //ventana principal de la aplicación
+            
+      //GameTester test = GameTester.getInstance();
+      
+      // Poner el numero de jugadores con el que se quiera probar
+      //test.play(game, 2); 
+              
+    }
+}
