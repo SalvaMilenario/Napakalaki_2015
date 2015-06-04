4419c9d4918debe48375f0f8c6dfc496e9bcbade
xehartnort <xehartnort@optimus>
2015-06-04 12:40:51 +0200

    Creado modelo de vista Napakalaki

--- Napakalaki/src/GUI/NapakalakiView.java
+++ Napakalaki/src/GUI/NapakalakiView.java
@@ -40,11 +40,11 @@ public class NapakalakiView extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 608, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 382, Short.MAX_VALUE)
         );
 
         pack();
